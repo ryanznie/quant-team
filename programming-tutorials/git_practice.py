7a40417d79b032f418e5b8d60dea79aeb686e58f
ryanznie <ryanznie@gmail.com>
2023-02-16 12:02:03 -0500

    added git practice file

---
+++ programming-tutorials/git_practice.py
@@ -0,0 +1,14 @@
+"""
+Quant - Example .py file to practice Git
+"""
+
+# Sample code
+
+def Square(x):
+    """
+    returns the square of input x
+    """
+    return x**2
+
+x = 3
+print(f"The square of {x} is {Square(x)}.")
